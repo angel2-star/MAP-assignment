ed42d4cb800e0a94817bbaebfc9e20b54e19461b
Driftedbucket <ndinowix@gmail.com>
2026-05-06 15:52:39 +0200

    Query definitions for the Task Entity

--- app/src/main/java/com/example/valentinesgarage/data/dao/TaskDao.kt
+++ app/src/main/java/com/example/valentinesgarage/data/dao/TaskDao.kt
@@ -9,5 +9,19 @@ import com.example.valentinesgarage.data.entity.TaskEntity
 
 @Dao
 interface TaskDao{
+    @Insert
+    suspend fun insert(task: TaskEntity)
+
+    @Update
+    suspend fun update(task: TaskEntity)
+
+    @Query("SELECT * FROM tasks WHERE vehicleId = :vehicleId")
+    fun getTasksForVehicle(vehicleId: Int): LiveData<List<TaskEntity>>
+
+    @Query("SELECT * FROM tasks WHERE assignedToUserId = :userId AND isCompleted = 1")
+    fun getCompletedTasksByUser(userId: Int): LiveData<List<TaskEntity>>
+
+    @Query("SELECT * FROM tasks WHERE isCompleted = 1")
+    fun getAllCompletedTasks(): LiveData<List<TaskEntity>>
 
 }
