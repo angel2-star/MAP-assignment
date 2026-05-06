b4f322122d1becd5970411ad9ac5b868f66bcb67
Driftedbucket <ndinowix@gmail.com>
2026-05-06 10:12:28 +0200

    User table with its attributes

--- app/src/main/java/com/example/valentinesgarage/data/entity/UserEntity.kt
+++ app/src/main/java/com/example/valentinesgarage/data/entity/UserEntity.kt
@@ -2,3 +2,12 @@ package com.example.map-assignment.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val email: String,
+    val password: String,
+    val role: String        //"mechanic" or "manager"
+)
