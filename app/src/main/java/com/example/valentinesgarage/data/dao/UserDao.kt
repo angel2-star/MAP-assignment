359bc1c75b088a2adbfe8d27650c2bb64e800a99
Driftedbucket <ndinowix@gmail.com>
2026-05-06 15:44:33 +0200

    Database imports and interface declration

---
+++ app/src/main/java/com/example/valentinesgarage/data/dao/UserDao.kt
@@ -0,0 +1,11 @@
+package com.example.map-assignment.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.map-assignment.data.entity.UserEntity
+
+@Dao
+interface UserDao{
+
+}
