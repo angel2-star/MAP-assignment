9f2b880b0eefa76a9a0374234f81e2fefff98c11
Driftedbucket <ndinowix@gmail.com>
2026-05-06 15:46:56 +0200

    Database imports and interface declration

---
+++ app/src/main/java/com/example/valentinesgarage/data/dao/VehicleDao.kt
@@ -0,0 +1,12 @@
+package com.example.map-assignment.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.map-assignment.data.entity.VehicleEntity
+
+@Dao
+interface VehicleDao{
+
+}
