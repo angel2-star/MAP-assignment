1354bfe5b314978328e4b55d5bdcf728868909f7
Driftedbucket <ndinowix@gmail.com>
2026-05-06 15:48:57 +0200

    Database imports and interface declration

---
+++ app/src/main/java/com/example/valentinesgarage/data/dao/TaskDao.kt
@@ -0,0 +1,13 @@
+package com.example.map-assignment.data.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.valentinesgarage.data.entity.TaskEntity
+
+@Dao
+interface TaskDao{
+
+}
